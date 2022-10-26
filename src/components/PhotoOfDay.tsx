314b60685115887777560230ee7c7ed9c5864c57
tsivkadi <tsivkadi@gmail.com>
2022-10-26 16:55:31 +0300

    upd

--- src/components/PhotoOfDay.tsx
+++ src/components/PhotoOfDay.tsx
@@ -26,7 +26,7 @@ function PhotoOfDay() {
         <span>{photo.date}</span>
       </Title>
       <Content>
-        {photo.media_type == 'image' ? <img src={photo.hdurl} alt='photo'/> : <iframe width="690" height="500"
+        {photo.media_type == 'image' ? <img src={photo.hdurl} alt='photo' width="690"/> : <iframe width="690" height="500"
         src={photo.url}>
         </iframe>}
       </Content>
